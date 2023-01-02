92a9de34fc79e96d4d9b301d4f34a493c41d6fd9
Kradaz <adarshsaulick@hotmail.it>
2023-01-02 17:32:48 +0000

    Prop-types
    Prop-types installed
    Api linked

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,31 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Shutter Island",
-      image:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTDqBjsQqiH8fHLE7rmp8Kesh4143AeyVOfnItvVPRpGMMSLfsx",
-      author: "Martin Scorsese"
-    },
-    {
-      id: 2,
-      title: "Seven Souls",
-      image:
-        "https://scontent-lhr8-1.xx.fbcdn.net/v/t39.30808-6/301750213_423737133189593_4628249996821775683_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=d75-5eC3I2oAX-fzCeB&_nc_ht=scontent-lhr8-1.xx&oh=00_AfDOPSpvPjCEOMDTaXXxUYYtkCsIgwjMyLQ9n3WYOT4qxg&oe=63B1C4B0",
-      author: "Gerry Bruno"
-    },
-    {
-      id: 3,
-      title: "Trainspotting",
-      image:
-        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRL72QB6PzUUQZMxsfFNBtctU5XpRUOJDPL2r7XnT1iPl39yCMH",
-      author: "Danny Boyle"
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://lit-headland-72819.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+            author: doc.author_name?.[0]
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
