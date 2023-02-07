b61468269df1b15499afa70efe9cf73218452f47
Kradaz <adarshsaulick@hotmail.it>
2023-02-07 18:37:11 +0000

    brought back code from 3.4
    code of 3.4 re-implemented back

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,19 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
+/* import { LoginView } from "../login-view/login-view"; */
 
 export const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
-  const [user, setUser] = useState(storedUser? storedUser : null);
-  const [token, setToken] = useState(storedToken? storedToken : null);
+/*   const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token"); */
+  // const [user, setUser] = useState(storedUser? storedUser : null);
+/*   const [token, setToken] = useState(storedToken? storedToken : null); */
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   
 
   useEffect(() => {
-    if (!token) return;
-
-    fetch("https://lit-headland-72819.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` },
-    })
+    fetch("https://lit-headland-72819.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((doc) => {
@@ -31,31 +27,18 @@ export const MainView = () => {
 
         setMovies(moviesFromApi);
       });
-  }, [token]);
-
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
+  }, []);
 
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
   }
-
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
   return (
     <div>
-      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
